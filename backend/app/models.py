72154c5ce3d2b241d1d34db138cd8d7a4ddaca54
kajotello <kajotello@gmail.com>
2024-04-09 00:44:54 +0200

    change timeezone to utc in one more place

--- backend/app/models.py
+++ backend/app/models.py
@@ -23,8 +23,8 @@ class Journey(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     ship_tier = Column(Integer)
     duration = Column(Integer)
-    start_date = Column(DateTime, default=datetime.datetime.now(datetime.UTC))
-    end_date = Column(DateTime, default=datetime.datetime.now(datetime.UTC))
+    start_date = Column(DateTime, default=datetime.datetime.now(datetime.timezone.utc))
+    end_date = Column(DateTime, default=datetime.datetime.now(datetime.timezone.utc))
     end_type = Column(Integer, default=0)
     experience_to_get = Column(Integer)
 
